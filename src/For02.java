5246572c3bc42956c110ae63012f70e871fca60b
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-02-12 22:30:41 +0900

    課題提出

---
+++ src/For02.java
@@ -0,0 +1,15 @@
+
+public class For02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int[] array = {1,2,3,4,5};
+		
+		for(int number : array){
+			System.out.println("For02 = " + number);
+		}
+
+	}
+
+}
